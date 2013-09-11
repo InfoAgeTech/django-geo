6b3bb115ad004d3a8c9798f6780a13ece1757bf2
Troy Grosfield <troy.grosfield@gmail.com>
2013-09-11 00:04:30 -0600

    Added timezone middleware

---
+++ django_geo/middleware.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from django.utils import timezone
+
+
+class TimezoneMiddleware(object):
+    """Middleware to get a users timezone if one exists.  That way datetime can
+    always be displayed according to either the users preferred setting or 
+    the browser's setting.
+    
+    @see: https://docs.djangoproject.com/en/dev/topics/i18n/timezones/#selecting-the-current-time-zone
+    """
+    def process_request(self, request):
+        tz = request.session.get('user_timezone')
+
+        if tz:
+            timezone.activate(tz)
