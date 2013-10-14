4e3ffc1ec7c60505bdcd81b611fe82124ebbb352
Troy Grosfield <troy.grosfield@gmail.com>
2013-10-14 07:01:07 -0600

    added location model mixin.

---
+++ django_geo/mixins/models.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from ..models import Location
+
+
+class AbstractLocationModelMixin(models.Model):
+    """Model mixin for location based models."""
+    location = models.ForeignKey(Location,
+                                 blank=True,
+                                 null=True,
+                                 on_delete=models.SET_NULL)
+
+    class Meta:
+        abstract = True
