dc9e72f10fc76184acfea2d5f4b832532d5df034
Troy Grosfield <troy.grosfield@gmail.com>
2014-09-04 17:43:05 -0600

    dropped support for django < 1.6

--- tests/settings.py
+++ tests/settings.py
@@ -1,24 +1,16 @@
-# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 import os
 
 # Do not run in DEBUG in production!!!
 DEBUG = False
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'UTC'
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
+ALLOWED_HOSTS = ['*']
 LANGUAGE_CODE = 'en-us'
-
+SECRET_KEY = '12345abcd'
 SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
+TIME_ZONE = 'UTC'
 USE_I18N = True
 
 INSTALLED_APPS = (
@@ -32,14 +24,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
-
-# Added in django 1.5 secret key is required.  This is a random generated string
-SECRET_KEY = '12345abcd'
-
-# Added in django 1.4.4. See: https://docs.djangoproject.com/en/1.4/releases/1.4.4/#host-header-poisoning
-ALLOWED_HOSTS = ['*']
-
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 here = lambda *x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
 
